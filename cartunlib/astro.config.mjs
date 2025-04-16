e22868086720d35dcde6f474d54772349f70633c
ArTiKYARE <crahercenter@gmail.com>
2025-04-17 02:32:45 +0600

    Добавленны страницы сша россии и британии

--- cartunlib/astro.config.mjs
+++ cartunlib/astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/',  // Если сайт в корне домена
+  output: 'static',  // TimeWeb поддерживает только статику
+  trailingSlash: 'ignore',  // Автоматически обрабатывает слэши
+});
